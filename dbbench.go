f55fa8ae39f6c4ba6dbcf8b031586abe003f6972
Nikolay Markov <markov@alignedresearch.com>
2019-12-18 23:49:32 +0300

    Vertica support added

--- dbbench.go
+++ dbbench.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
+	_ "github.com/vertica/vertica-sql-go"
 	"golang.org/x/net/context"
 	"log"
 	"os"
--- sql_database.go
+++ sql_database.go
@@ -232,3 +232,13 @@ func sqlServerDataSourceName(cc *ConnectionConfig) string {
 		firstString(cc.Database, ""),
 		firstString(cc.Params, ""))
 }
+
+func verticaDataSourceName(cc *ConnectionConfig) string {
+	return fmt.Sprintf("vertica://%s:%s@%s:%d/%s?%s",
+		firstString(cc.Username, "root"),
+		firstString(cc.Password, ""),
+		firstString(cc.Host, "localhost"),
+		firstInt(cc.Port, 5433),
+		firstString(cc.Database, ""),
+		firstString(cc.Params, ""))
+}
