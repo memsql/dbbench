cd29db8047cd41df5ece73c6fdf3e46501591082
Albert Vernon <avernon@memsql.com>
2020-11-03 14:03:20 -0500

    Mask passwords when printing debugging information

--- sql_database.go
+++ sql_database.go
@@ -153,8 +153,12 @@ func (sq *sqlDatabaseFlavor) QuerySeparator() string {
 }
 
 func (sq *sqlDatabaseFlavor) Connect(cc *ConnectionConfig) (Database, error) {
+	realPassword := cc.Password
+	cc.Password = "XXX" // Mask password before printing it.
 	dsn := sq.dsnFunc(cc)
 	log.Println("Connecting to", dsn)
+	cc.Password = realPassword
+	dsn = sq.dsnFunc(cc)
 
 	db, err := sql.Open(sq.name, dsn)
 	if err != nil {
